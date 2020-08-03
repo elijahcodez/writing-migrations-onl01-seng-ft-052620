136aae887fb8632cb45445a69231c45a5c763f18
Elijah Kirschen <ekirschen584@gmail.com>
2020-08-03 20:49:47 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,8 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
-  def change
-    create_table :students do |t|
-      t.integer :grade
-      t.string :birthdate
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+
+    def change
+        add_column :students, :grade, :integer
+        add_column :students, :birthdate, :string
     end
-  end
+
 end
